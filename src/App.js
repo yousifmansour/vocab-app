f46fb03f13fffdc06708603682d3fb78c68e9ca3
Yousif <yousifyacoub1997@gmail.com>
2018-12-19 18:12:14 +0200

    fix issues with keys of items in list and some more display stuff of the data model

--- src/App.js
+++ src/App.js
@@ -7,110 +7,110 @@ import DisplayDefinition from './DisplayDefinition.js';
 import ModeLayer from './ModeLayer.js';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      entries: [],
-      currentWord: '',
-      currentMode: 'learn',
-      status: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            entries: [],
+            currentWord: '',
+            currentMode: 'learn',
+            status: ''
+        }
+        this.handleInput = this
+            .handleInput
+            .bind(this);
+
+        this.updateMode = this
+            .updateMode
+            .bind(this);
     }
-    this.handleInput = this
-      .handleInput
-      .bind(this);
 
-    this.updateMode = this
-      .updateMode
-      .bind(this);
-  }
-
-  handleInput(input) {
-    if (input && input.length >= 1) {
-      this.setState({
-        entries: [],
-        currentWord: ''
-      }, () => {        
-        let url = 'https://dictionaryapi.com/api/v3/references/collegiate/json/<WORD>?key=51ea8a10-' +
-            '9f05-41e9-9adb-fff0f8785ac7';
-        fetch(url.replace('<WORD>', input)).then((data) => data.json()).then((data) => {
-          this.parseResult(data, input);
-        }).catch((e) => {
-          this.setState({entries: [], currentWord: input, status: 'not found'});
-        });
-      });
-
-    } else {
-      alert('Enter something');
-    }
-  }
-
-  parseResult(data, input) {
-  
-    let entries = [];
-    let sensesArray;
-    let senses;
-
-    data.forEach((entry) => {
-      sensesArray = [];
-      let fl = entry.fl;
-      entry
-        .def
-        .forEach((def) => {
-          def
-            .sseq
-            .forEach((sseq) => {
-
-              senses = [];
-              sseq.forEach((sense) => {
-                if (sense[0] !== 'sense') 
-                  return;
-                
-                let dt = sense[1].dt;
-
-                dt.forEach((element) => {
-                  let text = '';
-                  let vis = '';
-                  if (element[0] === 'text') 
-                    text = element[1];
-                  if (element[0] === 'vis') 
-                    vis = element[1][0].t;
-                  senses.push({text, vis});
+    handleInput(input) {
+        if (input && input.length >= 1) {
+            this.setState({
+                entries: [],
+                currentWord: ''
+            }, () => {
+                let url = 'https://dictionaryapi.com/api/v3/references/collegiate/json/<WORD>?key=51ea8a10-' +
+                        '9f05-41e9-9adb-fff0f8785ac7';
+                fetch(url.replace('<WORD>', input)).then((data) => data.json()).then((data) => {
+                    this.parseResult(data, input);
+                }).catch((e) => {
+                    this.setState({entries: [], currentWord: input, status: 'not found'});
                 });
-                sensesArray.push(senses);
-              });
             });
-        });
 
-      entries.push({sensesArray, fl});
-    });    
-    this.setState({entries, currentWord: input, status: 'found'});
-  }
-
-  updateMode(mode) {
-    if (mode === 'learn') 
-      this.setState({currentMode: 'learn'});
-    else if (mode === 'search') 
-      this.setState({currentMode: 'search'});
-    else {
-      alert('Something was wrong.');
-      alert(mode);
+        } else {
+            alert('Enter something');
+        }
     }
-  }
 
-  render() {
-    return (
-      <div className='App'>
-        <Navbar updateMode={this.updateMode} selected={this.state.currentMode}/>
+    parseResult(data, input) {
 
-        <ModeLayer
-          currentWord={this.state.currentWord}
-          handleInput={this.handleInput}
-          currentMode={this.state.currentMode}/>
+        let entries = [];
+        let sensesArray;
+        let senses;
 
-        <DisplayDefinition entries={this.state.entries}/>
-      </div>
-    );
-  }
+        data.forEach((entry) => {
+            sensesArray = [];
+            let fl = entry.fl;
+            entry
+                .def
+                .forEach((def) => {
+                    def
+                        .sseq
+                        .forEach((sseq) => {
+
+                            senses = [];
+                            sseq.forEach((sense) => {
+                                if (sense[0] !== 'sense') 
+                                    return;
+                                
+                                let dt = sense[1].dt;
+
+                                dt.forEach((element) => {
+                                    let text = '';
+                                    let vis = '';
+                                    if (element[0] === 'text') 
+                                        text = element[1];
+                                    if (element[0] === 'vis') 
+                                        vis = element[1][0].t;
+                                    senses.push({text, vis});
+                                });
+                                sensesArray.push(senses);
+                            });
+                        });
+                });
+
+            entries.push({sensesArray, fl});
+        });
+        this.setState({entries, currentWord: input, status: 'found'});
+    }
+
+    updateMode(mode) {
+        if (mode === 'learn') 
+            this.setState({currentMode: 'learn'});
+        else if (mode === 'search') 
+            this.setState({currentMode: 'search'});
+        else {
+            alert('Something was wrong.');
+            alert(mode);
+        }
+    }
+
+    render() {
+        return (
+            <div className='App'>
+                <Navbar updateMode={this.updateMode} selected={this.state.currentMode}/>
+
+                <ModeLayer
+                    currentWord={this.state.currentWord}
+                    handleInput={this.handleInput}
+                    currentMode={this.state.currentMode}/>
+
+                <DisplayDefinition entries={this.state.entries}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/DisplayEntry.js
+++ src/DisplayEntry.js
@@ -8,40 +8,41 @@ import Example from './Example.js';
 
 class DisplayEntry extends React.Component {
 
-  static propTypes = {
-    id: PropTypes.number.isRequired,
-    functionalLabel: PropTypes.string.isRequired,
-    sensesArray: PropTypes
-      .arrayOf(PropTypes.array)
-      .isRequired
-  }
-
-  render() {
-    let elements = [];
-    let counter = 1; // counting # of defintions
-    if (this.props.sensesArray) {
-      this
-        .props
-        .sensesArray
-        .forEach((element) => {
-          element.forEach((def, i) => {
-            if (def.text !== '') {
-              elements.push(<Definition key={i} id={counter} value={def.text}/>);
-              counter++;
-            } else if (def.vis !== '') 
-              elements.push(<Example key={i} value={def.vis}/>);
-            }
-          );
-        });
+    static propTypes = {
+        id: PropTypes.number.isRequired,
+        functionalLabel: PropTypes.string.isRequired,
+        sensesArray: PropTypes
+            .arrayOf(PropTypes.array)
+            .isRequired
     }
 
-    return (
-      <div className="entry-container">
-        <h2 className="header">{this.props.id}) {this.props.functionalLabel}</h2>
-        {elements}
-      </div>
-    );
-  }
+    render() {
+        let elements = [];
+        let defCount = 1; // counting # of defintions
+        let exCount = -1; // counting # of examples
+        if (this.props.sensesArray) {
+            this
+                .props
+                .sensesArray
+                .forEach((element) => {
+                    element.forEach((def) => {
+                        if (def.text !== '') {
+                            elements.push(<Definition key={defCount} id={defCount} value={def.text}/>);
+                            defCount++;
+                        } else if (def.vis !== '') 
+                            elements.push(<Example key={exCount} value={def.vis}/>);
+                        exCount--;
+                    });
+                });
+        }
+
+        return (
+            <div className="entry-container">
+                <h2 className="header">{this.props.id}) {this.props.functionalLabel}</h2>
+                {elements}
+            </div>
+        );
+    }
 }
 
 export default DisplayEntry;
