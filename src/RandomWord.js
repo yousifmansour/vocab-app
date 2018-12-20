f429363b9bfa8cf2b9ee183935bf8fbae9839f7f
Yousif <yousifyacoub1997@gmail.com>
2018-12-20 22:36:21 +0200

    connect to server on vm

--- src/RandomWord.js
+++ src/RandomWord.js
@@ -3,49 +3,49 @@ import React from 'react';
 
 class RandomWord extends React.Component {
 
-  componentDidMount() {
-    if (this.props.word === '' || this.props.word === undefined) 
-      this.getNewWord();
+    componentDidMount() {
+        if (this.props.word === '' || this.props.word === undefined) 
+            this.getNewWord();
+        }
+    
+    getNewWord() {
+        let url = 'https://www.yousifmansour.space/api/vocab/get/random';
+        fetch(url).then((data) => (data.json())).then((data) => {
+            this
+                .props
+                .handleInput(data.word);
+        });
     }
-  
-  getNewWord() {
-    let url = 'http://localhost:5678/api/get/_random';
-    fetch(url).then((data) => (data.json())).then((data) => {
-      this
-        .props
-        .handleInput(data.word);
-    });
-  }
 
-  render() {
-    const word = this.props.word;
+    render() {
+        const word = this.props.word;
 
-    // this is the loading animation
-    const loader = <div className="loader loader-2">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>;
-    return (
+        // this is the loading animation
+        const loader = <div className="loader loader-2">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>;
+        return (
 
-      <div className="random-word">
-        {this.props.word
-          ? <div>
-              <h1>
-                {word}
-              </h1>
-              <button
-                onClick={(e) => {
-                e.preventDefault();
-                this.getNewWord();
-              }}>Get Random Word</button>
+            <div className="random-word">
+                {this.props.word
+                    ? <div>
+                            <h1>
+                                {word}
+                            </h1>
+                            <button
+                                onClick={(e) => {
+                                e.preventDefault();
+                                this.getNewWord();
+                            }}>Get Random Word</button>
+                        </div>
+                    : loader}
+
             </div>
-          : loader}
-
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default RandomWord;
